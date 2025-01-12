4f5b94b74ab0e204bfc6d52695a488d4e5e6751b
vazquezea96 <vazquezeduardo1278@gmail.com>
2025-01-12 12:00:12 -0800

    initial commit

---
+++ chapter-2/main.js
@@ -0,0 +1,10 @@
+// alert('hello, world');
+console.log('Hello, world!');
+let age = 28;
+let year = 2025;
+console.log('My age is: ', age, 'years old.');
+console.log('The year is: ', year);
+
+const points = 100;
+// points = 50; // This will throw an error
+console.log(points);
