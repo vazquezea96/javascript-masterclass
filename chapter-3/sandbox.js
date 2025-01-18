fcf3d836d47c61c9106711adc8cf8cb9095d57aa
vazquezea96 <vazquezeduardo1278@gmail.com>
2025-01-17 20:48:29 -0800

    chapter 3 complete.

--- chapter-3/sandbox.js
+++ chapter-3/sandbox.js
@@ -1,6 +1,6 @@
 // control flow
 // use loops for cycling through data
- 
+
 // use conditions statements for checking conditions
 
 // for loops
@@ -39,33 +39,71 @@
 // } while (i < 5);
 
 // if statements
-const age = 26;
+// const age = 26;
 
-if (age > 20) {
-    // console.log('you are over 20 years old.');
-}
+// if (age > 20) {
+//     // console.log('you are over 20 years old.');
+// }
 
-const ninjas = ['shuan', 'ryu', 'chun-li', 'yoshi'];
+// const ninjas = ['shuan', 'ryu', 'chun-li', 'yoshi'];
 
-if (ninjas.length >= 4) {
-    // console.log("That's a lot of ninjas!");
-}
+// if (ninjas.length >= 4) {
+//     // console.log("That's a lot of ninjas!");
+// }
 
-const password = 'p@ss';
+// const password = 'p@ss';
 
-if (password.length >= 12 && password.includes('@')) {
-    console.log('that password is mighty strong!');
-} else if(password.length >= 8 || password.includes('@')) {
-    console.log('that password is strong enough!');
-}
-else {
-    console.log('password is not strong enough');
-}
+// if (password.length >= 12 && password.includes('@')) {
+//     console.log('that password is mighty strong!');
+// } else if(password.length >= 8 || password.includes('@')) {
+//     console.log('that password is strong enough!');
+// }
+// else {
+//     console.log('password is not strong enough');
+// }
 
 // logical NOT (!)
 
-let user = false;
+// let user = false;
 
-if (user) {
-    
+// if (user) {
+
+// }
+
+
+// break and continue
+
+// const scores = [50, 25, 0, 30, 100, 20, 10];
+
+// for(let i = 0; i < scores.length; i++) {
+
+//     if (scores[i] === 0) {
+//         continue;
+//     }
+
+//     console.log("your score: ", scores[i]);
+
+//     if(scores[i] === 100) {
+//         console.log("congrats, you got the top score!");
+//         break;
+//     }
+// }
+
+// variable and block scopes
+
+const age = 30;
+
+if (true) {
+    age = 40;
+    const name = 'eduardo';
+    console.log('inside 1st code block: ', age, name);
+
+    if (true) {
+        const age = 50;
+        console.log('inside 2nd code block: ', age);
+    }
 }
+
+console.log("outside code block: ", age, name);
+
+
