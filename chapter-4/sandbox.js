2a03362fc81bc46575bbe673dd70e6ff4fb8cd82
vazquezea96 <vazquezeduardo1278@gmail.com>
2025-01-31 22:22:44 -0800

    80% done with Chapter 4.

---
+++ chapter-4/sandbox.js
@@ -0,0 +1,45 @@
+// functions & methods
+// function declaration
+// function greet() {
+//     console.log('Hello World');
+// }
+
+// function expression 
+// const speak = function (name="Luigi", time="Night") {
+//     console.log(`Good ${time} ${name}!`);  
+// };
+
+
+// speak("Mario", "Morning");
+// speak(); // invokes the default value if none passed in.
+
+
+// const calcArea = function(radius) {
+//     return 3.14 * radius ** 2;
+// };
+
+// // Arrow Functions
+// const calcArea = radius => 3.14 * radius ** 2;
+
+// const area = calcArea(5);
+// console.log('area is: ', area);
+
+// Practice arrow functions
+
+// const greet = _ => `hello world`;
+
+// const greeting = greet();
+// console.log(greeting);
+
+// const bill = (products, tax) => {
+//     let total = 0;
+//     for (let i = 0; i < products.length; i++) {
+//         total += products[i] + products[i] * tax;
+//     }
+//     return total;
+// };
+
+// console.log(bill([10, 15, 30], 0.2));
+
+
+
