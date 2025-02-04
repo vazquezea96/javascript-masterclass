14eb6d4a05a4a2f95b85484f3272c31517bd5a26
vazquezea96 <vazquezeduardo1278@gmail.com>
2025-02-04 10:09:36 -0800

    Chapter 6 begins. DOM

---
+++ chapter-6/main.js
@@ -0,0 +1,8 @@
+// const para = document.querySelector('div.error');
+
+// console.log(para);
+
+const paras = document.querySelectorAll('p');
+const errors = document.querySelectorAll('.error');
+
+console.log(errors);
