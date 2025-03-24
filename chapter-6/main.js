bbae51e7db0e7d4c7ec784e9ce9daec742c75d96
vazquezea96 <vazquezeduardo1278@gmail.com>
2025-03-24 15:27:22 -0700

    Half way complete through lesson.

--- chapter-6/main.js
+++ chapter-6/main.js
@@ -2,7 +2,68 @@
 
 // console.log(para);
 
-const paras = document.querySelectorAll('p');
-const errors = document.querySelectorAll('.error');
+// const paras = document.querySelectorAll('p');
+// const errors = document.querySelectorAll('.error');
 
-console.log(errors);
+// console.log(errors);
+
+// get an element by ID
+// const title = document.getElementById('page-title');
+// console.log(title);
+
+// get element by their class name
+// const errors = document.getElementsByClassName('error');
+// console.log(errors);
+
+// get elements by their tag name
+// const paras = document.getElementsByTagName('p');
+// console.log(paras);
+// console.log(paras[1]);
+
+// const para = document.querySelector('p');
+
+// console.log(para.innerText);
+// para.innerText = 'ninjas are awesome!';
+
+// const paras = document.querySelectorAll('p');
+
+// paras.forEach(para => {
+//     console.log(para.innerText);
+//     para.innerText += ' new text';
+// });
+
+// const content = document.querySelector('.content');
+
+// console.log(content.innerHTML);
+// content.innerHTML += '<h2>This is a new H2</h2>'
+
+// const people = ['mario', 'luigi', 'yoshi'];
+
+// people.forEach(person => {
+//     content.innerHTML += `<p>${person}</p>`;
+// });
+
+// const link = document.querySelector('a');
+
+// console.log(link.getAttribute('href'));
+// link.setAttribute('href', 'https://www.thenetninja.co.uk');
+// link.innerText = "The Net Ninja Website";
+
+const mssg = document.querySelector('.error');
+
+// console.log(mssg.getAttribute('class'));
+
+// const title = document.querySelector('h1');
+
+// title.setAttribute('style', 'margin: 50px;');
+
+// console.log(title.style);
+// console.log(title.style.color);
+
+// title.style.margin = '50px';
+// title.style.color = 'crimson';
+// title.style.fontSize = '60px';
+
+// changing class
+console.log(mssg.classList);
+mssg.classList.add('error');
